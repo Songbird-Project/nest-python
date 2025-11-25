c1df41ee51be9a34ae11bb69b345f534191acfd6
vaelixd <vaelixd@proton.me>
2025-11-25 15:58:50 +1300

    update example to include all options

--- examples/config.py
+++ examples/config.py
@@ -1,10 +1,30 @@
 import nest
 
 def preBuild():
-    print("Hello, World!")
+    print("hello")
+
+def postBuild():
+    pass
 
 config = nest.newConfig()
 
+config.hostname = "vaelixd-pc"
+config.kernels = ["linux-zen", "linux", "linux-lts"]
+config.bootloader = "refind"
+config.initramfsGenerator = "dracut"
 config.preBuild = preBuild
+config.postBuild = postBuild
+
+vaelixd = nest.User(
+    fullName="vaelixd",
+    userName="vaelixd",
+    homeDir="/home/vaelixd",
+    manageHome=True,
+    groups=["vaelixd", "wheel"]
+)
+
+config.users = [
+    vaelixd
+]
 
 nest.returnConfig(config)
